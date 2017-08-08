40c29031b18cca36c4ee9a0d32dca201af6b98ae
Mikhail Kamnev <Mikhail Kamnev@localhost>
2017-08-08 17:55:53 +0700

    focus test for pages

--- MazesAndMinotaurs.Ui/Tests/FocusTests.cs
+++ MazesAndMinotaurs.Ui/Tests/FocusTests.cs
@@ -21,5 +21,26 @@ namespace MazesAndMinotaurs.Ui.Tests
 			border.IsFocused = true;
 			Assert.IsTrue(menu.IsFocused);
 		}
+
+		[Test]
+		public void FirstPageShouldBeFocusedAfterPagesFocused()
+		{
+			var border1 = new Border<char, object, TestKey>();
+			var border2 = new Border<char, object, TestKey>();
+			var pages = new Pages<char, object, TestKey>(new TestKeyboardAdapter());
+
+			pages.Controls.Add(border1);
+			pages.Controls.Add(border2);
+
+			Assert.IsFalse(border1.IsFocused);
+			Assert.IsFalse(border2.IsFocused);
+			Assert.IsFalse(pages.IsFocused);
+
+			pages.IsFocused = true;
+
+			Assert.IsTrue(border1.IsFocused);
+			Assert.IsFalse(border2.IsFocused);
+			Assert.IsTrue(pages.IsFocused);
+		}
 	}
 }
