5fd53ef3113828911b9a73801632d50040a8a34e
Mikhail Kamnev <Mikhail Kamnev@localhost>
2017-08-17 17:23:31 +0700

    movement tests

---
+++ Sokoban.Core/Tests/MovementTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Sokoban.Core.Tests
+{
+	[TestFixture]
+	internal class MovementTests
+	{
+		[Test]
+		public void BasicTest()
+		{
+			const string step0 = @"####
+#-@#
+#--#
+####";
+			const string step1 = @"####
+#@-#
+#--#
+####";
+			const string step2 = @"####
+#--#
+#@-#
+####";
+			const string step3 = @"####
+#--#
+#-@#
+####";
+			var level = new LevelCreator().Create(step0);
+			TestStep(level, level.TryMoveLeft, step1);
+			TestStep(level, level.TryMoveDown, step2);
+			TestStep(level, level.TryMoveRight, step3);
+			TestStep(level, level.TryMoveUp, step0);
+		}
+
+		[Test]
+		public void WallsShouldBeImpassable()
+		{
+			const string levelData = @"###
+#@#
+###";
+			NoMovement(levelData);
+		}
+
+		[Test]
+		public void CratesShouldBeImpassable()
+		{
+			const string levelData = @"#####
+#-$-#
+#$@$#
+#-$-#
+#####";
+			NoMovement(levelData);
+		}
+
+		public void CratesShouldBeMovable()
+		{
+			const string step0 = @"-###-
+-#-#-
+##$##
+#-$@#
+#####";
+			const string step1 = @"-###-
+-#-#-
+##$##
+#$@-#
+#####";
+			const string step2 = @"-###-
+-#$#-
+##@##
+#$--#
+#####";
+			var level = new LevelCreator().Create(step0);
+			TestStep(level, level.TryMoveLeft, step1);
+			TestStep(level, level.TryMoveUp, step2);
+		}
+
+		private static void NoMovement(string levelData)
+		{
+			var level = new LevelCreator().Create(levelData);
+			TestStep(level, level.TryMoveLeft, levelData, Assert.IsFalse);
+			TestStep(level, level.TryMoveDown, levelData, Assert.IsFalse);
+			TestStep(level, level.TryMoveRight, levelData, Assert.IsFalse);
+			TestStep(level, level.TryMoveUp, levelData, Assert.IsFalse);
+		}
+
+		private static void TestStep(Level level, Func<bool> stepFunc, string stepResult, Action<bool> assert = null)
+		{
+			assert = assert ?? Assert.IsTrue;
+			var moved = stepFunc();
+			assert(moved);
+			Assert.AreEqual(stepResult, level.ToString());
+		}
+	}
+}
